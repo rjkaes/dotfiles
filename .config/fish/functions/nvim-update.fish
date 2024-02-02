1c65927052e5c869aed4f1e30acf2e58a7cf43d7
Robert James Kaes <robert@kaes.ca>
2024-02-02 14:13:46 -0500

    fix: update path of nvim configs

--- .config/fish/functions/nvim-update.fish
+++ .config/fish/functions/nvim-update.fish
@@ -1,6 +1,6 @@
 function nvim-update
     nvim --headless "+Lazy! sync" +qa
-    pushd ~/.vim
+    pushd ~/.config/nvim/
     git add lazy-lock.json && git commit -m 'chore(plugins): update' && ggpush
     popd
 end
