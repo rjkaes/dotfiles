83e922c42a2cdaff31da7453e62bc1d730aeaa66
Robert James Kaes <robert@kaes.ca>
2024-02-11 08:42:28 -0500

    feat(keymap): diagnostics easier to see and navigate

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -45,6 +45,12 @@ vim.keymap.set('v', '<leader>y', '"+y')
 -- LSP formatting
 vim.keymap.set({'n', 'v'}, '<localleader>f', function() require("conform").format() end, { silent = true })
 
+-- Diagnostics
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+
 -- Git
 vim.keymap.set('n', '<leader>gb', ':Git blame<cr>')
 vim.keymap.set('n', '<leader>gci', ':Neogit kind=split_above<cr>')
