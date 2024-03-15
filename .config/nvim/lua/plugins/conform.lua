d6cabf556023374d7414ac45d4fa8e6e93e5ee84
Robert James Kaes <robert@kaes.ca>
2024-03-15 09:17:16 -0400

    feat: add t-sql formatting via sql_formatter

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,6 +1,11 @@
 local conform = require("conform");
 local util = require("conform.util");
 
+-- format microsoft t-sql
+conform.formatters.sql_formatter = {
+    prepend_args = { "-l", "tsql", "-c", '{ "tabWidth": 2, "keywordCase": "upper", "dataTypeCase": "upper", "linesBetweenQueries": 2, "dialect": "transactsql" }' },
+}
+
 conform.setup({
     formatters_by_ft = {
         cs = { "csharpier" },
@@ -9,6 +14,7 @@ conform.setup({
         json = { { "biome", "prettier" } },
         ruby = { "standardrb" },
         rust = { "rustfmt" },
+        sql = { "sql_formatter" },
         ["_"] = { "trim_whitespace" },
     },
 });
