3e90f3cdf054f4391cf28843a3f45f82a0edd9d1
Robert James Kaes <robert@kaes.ca>
2024-04-25 11:38:08 -0400

    fix: remove redundant `opts` for noice and enable search

--- dot-config/nvim/lua/plugins/lazy.lua
+++ dot-config/nvim/lua/plugins/lazy.lua
@@ -67,9 +67,6 @@ require("lazy").setup({
     {
         "folke/noice.nvim",
         event = "VeryLazy",
-        opts = {
-            -- add any options here
-        },
         dependencies = {
             -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
             "MunifTanjim/nui.nvim",
@@ -85,7 +82,7 @@ require("lazy").setup({
             },
             -- you can enable a preset for easier configuration
             presets = {
-                bottom_search = true, -- use a classic bottom cmdline for search
+                bottom_search = false, -- use a classic bottom cmdline for search
                 command_palette = true, -- position the cmdline and popupmenu together
                 long_message_to_split = true, -- long messages will be sent to a split
                 inc_rename = false, -- enables an input dialog for inc-rename.nvim
