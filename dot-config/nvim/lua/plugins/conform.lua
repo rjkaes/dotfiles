7c9a5ab986146f41e8418c7a351f5196579d816a
Robert James Kaes <robert@kaes.ca>
2024-04-29 09:50:23 -0400

    feat(conform): auto-format files on save

--- dot-config/nvim/lua/plugins/conform.lua
+++ dot-config/nvim/lua/plugins/conform.lua
@@ -17,4 +17,12 @@ conform.setup({
         sql = { "sql_formatter" },
         ["_"] = { "trim_whitespace" },
     },
+    -- If this is set, Conform will run the formatter on save.
+    -- It will pass the table to conform.format().
+    -- This can also be a function that returns the table.
+    format_on_save = {
+        -- I recommend these options. See :help conform.format for details.
+        lsp_fallback = true,
+        timeout_ms = 1000,
+    },
 });
