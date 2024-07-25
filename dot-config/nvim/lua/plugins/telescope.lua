b0966fc444531eb6c7b339fd963e2f58c6b50203
Robert James Kaes <robert@kaes.ca>
2024-07-25 09:36:25 -0400

    fix(vim): show hidden directories in telescope find_files
    See: https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#file-and-text-search-in-hidden-files-and-directories

--- dot-config/nvim/lua/plugins/telescope.lua
+++ dot-config/nvim/lua/plugins/telescope.lua
@@ -1,9 +1,25 @@
 local telescope = require('telescope')
+local telescopeConfig = require("telescope.config")
+local builtin = require("telescope.builtin")
+
+-- Clone the default Telescope configuration
+local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+-- I want to search in hidden/dot files.
+table.insert(vimgrep_arguments, "--hidden")
+-- I don't want to search in the `.git` directory.
+table.insert(vimgrep_arguments, "--glob")
+table.insert(vimgrep_arguments, "!**/.git/*")
 
 telescope.setup({
+    defaults = {
+        -- `hidden = true` is not supported in text grep commands.
+        vimgrep_arguments = vimgrep_arguments,
+    },
     pickers = {
         find_files = {
-            find_command = { "fd", "--type", "f", "--strip-cwd-prefix" }
+            -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+            find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
         },
     },
     extensions = {
@@ -16,7 +32,7 @@ telescope.setup({
 
 pcall(telescope.load_extension('fzy_native'))
 
-vim.keymap.set('n', "<C-p>", require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', "<leader>j", require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', "<leader>f", require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', "<C-p>", builtin.find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', "<leader>j", builtin.buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', "<leader>f", builtin.live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', "<leader>t", "<cmd>Telescope tags<cr>")
