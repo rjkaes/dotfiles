11157acb1cfe3845a0a611d2d77594100490497d
Robert James Kaes <robert@kaes.ca>
2025-03-12 10:17:02 -0400

    add omnisharp handlers for definitions, references, and implementations

--- dot-config/nvim/lua/plugins/lsp.lua
+++ dot-config/nvim/lua/plugins/lsp.lua
@@ -72,13 +72,22 @@ require('mason-lspconfig').setup({
 
         -- dotnet
         omnisharp = function()
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+            -- Add textDocument/hover capability
+            capabilities.textDocument.hover.contentFormat = { 'markdown', 'plaintext' }
+
             lspconfig.omnisharp.setup({
                 handlers = {
-                    ["textDocument/definition"] = require('omnisharp_extended').handler,
+                    ["textDocument/definition"] = require('omnisharp_extended').definition_handler,
+                    ["textDocument/typeDefinition"] = require('omnisharp_extended').type_definition_handler,
+                    ["textDocument/references"] = require('omnisharp_extended').references_handler,
+                    ["textDocument/implementation"] = require('omnisharp_extended').implementation_handler,
                 },
                 enable_roslyn_analyzers = true,
                 organize_imports_on_format = true,
                 enable_import_completion = true,
+                capabilities = capabilities,
             })
         end,
 
