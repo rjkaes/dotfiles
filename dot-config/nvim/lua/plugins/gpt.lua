66f8fef400280f92387d0c8da5ef658a1a969d76
Robert James Kaes <robert@kaes.ca>
2025-03-12 21:51:42 -0400

    update gpt code completion and chat
    Only run code completion when not on battery.  Running it constantly
    uses too much battery for not enough benefit.
    
    Also, switch to the q5_K_M models for improved accuracy.

--- dot-config/nvim/lua/plugins/gpt.lua
+++ dot-config/nvim/lua/plugins/gpt.lua
@@ -5,7 +5,7 @@ require("codecompanion").setup({
                 name = "coder",
                 schema = {
                     model = {
-                        default = "qwen2.5-coder:14b",
+                        default = "qwen2.5-coder:32b-instruct-q5_K_M",
                     },
                     num_ctx = {
                         default = 8192,
@@ -36,25 +36,40 @@ require("codecompanion").setup({
     },
 })
 
+local function is_on_battery()
+    local handle = io.popen("uname")
+    local os_name = handle:read("*l")
+    handle:close()
+
+    if os_name ~= "Darwin" then
+        return false -- Assume not on battery for non-MacOS systems
+    end
+
+    handle = io.popen("pmset -g batt | grep 'Battery Power'")
+    local result = handle:read("*a")
+    handle:close()
+    return result ~= ""
+end
+
 require("minuet").setup({
     provider = 'openai_fim_compatible',
     n_completions = 1, -- recommend for local model for resource saving
-    context_window = 512,
+    context_window = 1024,
     provider_options = {
         openai_fim_compatible = {
             api_key = 'TERM',
             name = 'Ollama',
             end_point = 'http://localhost:11434/v1/completions',
-            model = 'qwen2.5-coder:7b-base-q6_K',
+            model = 'qwen2.5-coder:14b-instruct-q4_K_M',
             optional = {
                 max_tokens = 256,
                 top_p = 0.9,
             },
         },
     },
-    virtualtext = {
+    virtualtext = not is_on_battery() and {
         auto_trigger_ft = { '*' },
-        auto_trigger_ignore_ft = { 'TelescopePrompt' },
+        auto_trigger_ignore_ft = { 'codecompanion', 'TelescopePrompt' },
         keymap = {
             -- accept whole completion
             accept = '<C-1>',
@@ -63,7 +78,7 @@ require("minuet").setup({
             dismiss = '<C-x>',
         },
         show_on_completion_menu = false,
-    },
+    } or nil,
 })
 
-vim.keymap.set('n', '<leader>C', ':Minuet virtualtext enable<CR>')
+vim.keymap.set('n', '<leader>C', ':Minuet virtualtext toggle<CR>')
