1f9d0f52e7475f3cd741518f3e05c587f5443524
Robert James Kaes <robert@kaes.ca>
2025-07-08 14:26:44 -0400

    fix: update codecompanion to use deepseek-coder-v2
    Remove codeium as it's no longer used and update codecompanion to
    default to deepseek-coder-v2.

--- dot-config/nvim/lua/plugins/gpt.lua
+++ dot-config/nvim/lua/plugins/gpt.lua
@@ -1,13 +1,56 @@
 require("codecompanion").setup({
+    adapters = {
+        opts = {
+            show_defaults = false,
+        },
+        deepseek = function()
+            return require("codecompanion.adapters").extend("ollama", {
+                name = "deekseek",
+                schema = {
+                    model = {
+                        default = "deepseek-coder-v2:16b",
+                    },
+                    num_ctx = {
+                        default = 40960,
+                    },
+                    max_tokens = {
+                        default = 40960,
+                    },
+                },
+            })
+        end,
+        qwencoder = function()
+            return require("codecompanion.adapters").extend("ollama", {
+                name = "qwencoder",
+                schema = {
+                    model = {
+                        default = "qwen2.5-coder:32b-instruct-q5_K_M",
+                    },
+                    num_ctx = {
+                        default = 16384,
+                    },
+                    -- temperature = {
+                    --     default = 0.6,
+                    -- },
+                    -- top_p = {
+                    --     default = 0.95,
+                    -- },
+                    max_tokens = {
+                        default = 32768,
+                    },
+                },
+            })
+        end,
+    },
     strategies = {
         chat = {
-            adapter = "copilot",
+            adapter = "deepseek",
         },
         inline = {
-            adapter = "copilot",
+            adapter = "deepseek",
         },
         agent = {
-            adapter = "copilot",
+            adapter = "deepseek",
         },
     },
 })
