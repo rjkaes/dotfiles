1354b80d36722fe7591765b6e2fa0e2c45ce7934
Robert James Kaes <robert@kaes.ca>
2025-08-30 09:50:07 -0400

    fix: upgrade codecompanion custom config
    codecompanion deprecated the old configuration. Upgrade to the new
    config and default to qwencoder for the AI chat buffer.
    
    NOTE: I might remove this altogether as I don't use "AI" as much as I
    had expected.

--- dot-config/nvim/lua/plugins/gpt.lua
+++ dot-config/nvim/lua/plugins/gpt.lua
@@ -1,50 +1,61 @@
 require("codecompanion").setup({
     adapters = {
-        opts = {
-            show_defaults = false,
+        http = {
+            opts = {
+                show_defaults = false,
+            },
+            deepseek = function()
+                return require("codecompanion.adapters").extend("ollama", {
+                    name = "deekseek",
+                    schema = {
+                        model = {
+                            default = "deepseek-coder-v2:16b",
+                        },
+                        num_ctx = {
+                            default = 65536,
+                        },
+                        max_tokens = {
+                            default = 65536,
+                        },
+                        top_p = {
+                            default = 0.95,
+                        },
+                    },
+                })
+            end,
+            qwencoder = function()
+                return require("codecompanion.adapters").extend("ollama", {
+                    name = "qwencoder",
+                    opts = {
+                        stream = true,
+                    },
+                    schema = {
+                        model = {
+                            default = "qwen3-coder:30b",
+                        },
+                        num_ctx = {
+                            default = 65536,
+                        },
+                        -- temperature = {
+                        --     default = 0.6,
+                        -- },
+                        top_p = {
+                            default = 0.95,
+                        },
+                        max_tokens = {
+                            default = 65536,
+                        },
+                        keep_alive = {
+                            default = '5m',
+                        },
+                    },
+                })
+            end,
         },
-        deepseek = function()
-            return require("codecompanion.adapters").extend("ollama", {
-                name = "deekseek",
-                schema = {
-                    model = {
-                        default = "deepseek-coder-v2:16b",
-                    },
-                    num_ctx = {
-                        default = 40960,
-                    },
-                    max_tokens = {
-                        default = 40960,
-                    },
-                },
-            })
-        end,
-        qwencoder = function()
-            return require("codecompanion.adapters").extend("ollama", {
-                name = "qwencoder",
-                schema = {
-                    model = {
-                        default = "qwen2.5-coder:32b-instruct-q5_K_M",
-                    },
-                    num_ctx = {
-                        default = 16384,
-                    },
-                    -- temperature = {
-                    --     default = 0.6,
-                    -- },
-                    -- top_p = {
-                    --     default = 0.95,
-                    -- },
-                    max_tokens = {
-                        default = 32768,
-                    },
-                },
-            })
-        end,
     },
     strategies = {
         chat = {
-            adapter = "deepseek",
+            adapter = "qwencoder",
         },
         inline = {
             adapter = "deepseek",
