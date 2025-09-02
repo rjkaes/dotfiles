ee0bee25baa9aeb9db40726fd2201e612131b3c8
Robert James Kaes <robert@kaes.ca>
2025-09-02 14:58:52 -0400

    feat: always enable treesitter when parser is available
    Treesitter is doing the heavy lifting for syntax highlighting.  Make
    sure it's enabled (and turn on folds as well) when the parser is
    available and installed.

--- dot-config/nvim/lua/plugins/treesitter.lua
+++ dot-config/nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,7 @@
 require('nvim-treesitter').setup({
     auto_install = true,
     disable = { 'markdown' },
-    ensure_installed = { 'lua', 'vim', 'ruby', "git_config", "gitcommit", "git_rebase", "gitignore", "gitattributes" },
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = { 'text', 'mail', 'markdown' },
-    },
+    ensure_installed = { 'lua', 'vim', 'ruby', "c_sharp", "git_config", "gitcommit", "git_rebase", "gitignore", "gitattributes" },
     incremental_selection = {
         enable = true,
         keymaps = {
@@ -15,7 +11,24 @@ require('nvim-treesitter').setup({
             node_decremental = "grm",
         },
     },
-    indent = {
-        enable = true,
-    },
+})
+
+-- Start treesitter if the parser is installed for the filetype when opening.
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function(args)
+    local ft = args.match
+    local ok, lang = pcall(vim.treesitter.language.get_lang, ft)
+    if not ok then
+      return
+    end
+
+    -- Check if parser is actually available
+    local has_parser = #vim.api.nvim_get_runtime_file("parser/" .. lang .. ".so", false) > 0
+    if has_parser then
+      pcall(vim.treesitter.start, args.buf, lang)
+
+      -- Enable folds
+      vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    end
+  end,
 })
