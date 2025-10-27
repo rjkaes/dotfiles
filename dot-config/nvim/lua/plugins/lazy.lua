759792e63a7695f34013217e5c506659ba4ed17e
Robert James Kaes <robert@kaes.ca>
2025-10-27 09:13:20 -0400

    fix: update leap as it no longer defines default keymaps

--- dot-config/nvim/lua/plugins/lazy.lua
+++ dot-config/nvim/lua/plugins/lazy.lua
@@ -210,7 +210,22 @@ require("lazy").setup({
         'ggandor/leap.nvim',
         lazy = false,
         config = function()
-            require('leap').add_default_mappings()
+            vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)')
+            vim.keymap.set('n',             'S', '<Plug>(leap-from-window)')
+
+            require('leap').opts.preview = function (ch0, ch1, ch2)
+                return not (
+                    ch1:match('%s')
+                    or (ch0:match('%a') and ch1:match('%a') and ch2:match('%a'))
+                )
+            end
+
+            -- Define equivalence classes for brackets and quotes, in addition to
+            -- the default whitespace group:
+            require('leap').opts.equivalence_classes = {
+                ' \t\r\n', '([{', ')]}', '\'"`'
+            }
+
         end,
     },
 
