4a9d0be28ddbfb5eb245948402fabce9c23fef11
Robert James Kaes <robert@kaes.ca>
2025-11-14 16:40:23 -0500

    chore: add typescriptreact to biome formatting

--- dot-config/nvim/lua/plugins/conform.lua
+++ dot-config/nvim/lua/plugins/conform.lua
@@ -24,6 +24,7 @@ conform.setup({
         rust = { "rustfmt" },
         sql = { "sqlfluff", "sql_formatter", stop_after_first = true },
         typescript = { "biome", "prettier", stop_after_first = true },
+        typescriptreact = { "biome", "prettier", stop_after_first = true },
         ["_"] = { "trim_whitespace" },
     },
     -- If this is set, Conform will run the formatter on save.
