8ecf307a8511163d129baa96deb4cb52bf8663ec
Robert James Kaes <robert@kaes.ca>
2026-02-25 10:21:02 -0500

    fix(nvim): update switch_to_razor_page function to use `vim.cmd`

--- dot-config/nvim/lua/autocommands.lua
+++ dot-config/nvim/lua/autocommands.lua
@@ -1,10 +1,9 @@
--- Switch between the cshtml Razor page and the backing (cshtml.cs) file.  Mimics vim-rails.
 local function switch_to_razor_page()
     local current_file = vim.fn.expand('%:p')
     if current_file:match('%.cshtml$') then
-        vim.cmd.edit(current_file:gsub('%.cshtml$', '.cshtml.cs'))
+        vim.cmd('edit ' .. current_file:gsub('%.cshtml$', '.cshtml.cs'))
     elseif current_file:match('%.cshtml%.cs$') then
-        vim.cmd.edit(current_file:gsub('%.cshtml%.cs$', '.cshtml'))
+        vim.cmd('edit ' .. current_file:gsub('%.cshtml%.cs$', '.cshtml'))
     end
 end
 
