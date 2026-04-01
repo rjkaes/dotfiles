6376805752e2a05d518c05f9195c5a51414e3e59
Robert James Kaes <robert@kaes.ca>
2026-04-01 15:49:29 -0400

    refactor(editor): remove leap.nvim
    Unused motion plugin. Frees up the `s`/`S` keys.

--- dot-config/nvim/lua/plugins/editor.lua
+++ dot-config/nvim/lua/plugins/editor.lua
@@ -21,27 +21,4 @@ return {
 
     -- Undo tree
     { 'mbbill/undotree', cmd = "UndotreeToggle" },
-
-    {
-        url = "https://codeberg.org/andyg/leap.nvim",
-        keys = {
-            { "s", mode = { "n", "x", "o" }, desc = "Leap" },
-            { "S", mode = "n", desc = "Leap from window" },
-        },
-        config = function()
-            vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)')
-            vim.keymap.set('n',             'S', '<Plug>(leap-from-window)')
-
-            require('leap').opts.preview = function (ch0, ch1, ch2)
-                return not (
-                    ch1:match('%s')
-                    or (ch0:match('%a') and ch1:match('%a') and ch2:match('%a'))
-                )
-            end
-
-            require('leap').opts.equivalence_classes = {
-                ' \t\r\n', '([{', ')]}', '\'"'
-            }
-        end,
-    },
 }
