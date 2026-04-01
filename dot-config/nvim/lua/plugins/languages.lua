63872dd35131df007d3183ddf4ceac74a8845764
Robert James Kaes <robert@kaes.ca>
2026-04-01 15:45:04 -0400

    refactor: update plugin dependencies for snacks.nvim migration
    - Remove telescope/dressing.nvim deps from codecompanion (snacks
      provides vim.ui.select and vim.ui.input)
    - Remove telescope dep from easy-dotnet
    - Remove manual nohlsearch keymap (auto-clear handles this)

--- dot-config/nvim/lua/plugins/languages.lua
+++ dot-config/nvim/lua/plugins/languages.lua
@@ -25,7 +25,7 @@ return {
     { 'jlcrochet/vim-razor', ft = { "razor", "cshtml" } },
     {
         "GustavEikaas/easy-dotnet.nvim",
-        dependencies = { "nvim-lua/plenary.nvim", 'nvim-telescope/telescope.nvim', },
+        dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
             require("easy-dotnet").setup()
         end
